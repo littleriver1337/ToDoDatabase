0e337dd468ebd98ca2b4f2bdb85634f4152fb259
MattBrown <litteriver1337@gmail.com>
2015-11-03 09:45:22 -0500

    Use h2 and removed ROWNUM and subsituted.

--- src/ToDo.java
+++ src/ToDo.java
@@ -1,3 +1,4 @@
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -6,23 +7,55 @@ import java.util.Scanner;
  */
 public class ToDo {
     static void printTodos(ArrayList<ToDoItem> todos) {
-        int todoNum = 1;
         for (ToDoItem todo : todos) {
             String checkBox = "[ ]";
             if (todo.isDone) {
                 checkBox = "[x]";
             }
-            String line = String.format("%d. %s %s", todoNum, checkBox, todo.text);
+            String line = String.format("%d. %s %s", todo.id, checkBox, todo.text);
             System.out.println(line);
-            todoNum++;
         }
     }
 
-    public static void main(String[] args) {
-        ArrayList<ToDoItem> todos = new ArrayList();
+    static void insertToDo(Connection conn, String text) throws SQLException {//creates insertToDo method for the values we are putting in table
+        PreparedStatement stmt = conn.prepareStatement("INSERT INTO todos VALUES (NULL, ?, false)");//null uses default number of IDENTITY column
+        stmt.setString(1, text);
+        stmt.execute();
+    }
+
+    static ArrayList<ToDoItem> selectToDos(Connection conn) throws SQLException { //returns all of the todos from the database
+        Statement stmt = conn.createStatement();
+        ResultSet results = stmt.executeQuery("SELECT * FROM todos");
+        ArrayList<ToDoItem> todos = new ArrayList(); //creates new arraylist
+        while (results.next()){//loops over the results of the todo list
+            int id = results.getInt("id");
+            String text = results.getString("text");
+            Boolean isDone = results.getBoolean("is_done");
+            ToDoItem item = new ToDoItem (id, text, isDone);
+            todos.add(item);
+        }
+        return todos;
+    }
+
+    static void toggleToDo(Connection conn, int selectNum) throws SQLException {
+        PreparedStatement stmt = conn.prepareStatement("UPDATE todos SET is_done = NOT is_done WHERE id = ?");
+        stmt.setInt(1, selectNum);//injects specific row number we want to use
+        stmt.execute();
+    }
+
+
+    public static void main(String[] args) throws SQLException {
+        Connection conn = DriverManager.getConnection("jdbc:h2:./main");
+        Statement stmt = conn.createStatement();
+        stmt.execute("CREATE TABLE IF NOT EXISTS todos (id IDENTITY, text VARCHAR, is_done BOOLEAN)");//IDENTITY is auto-incramenting
+
+
+
+        //ArrayList<ToDoItem> todos = new ArrayList(); removes this ad adds the arraylist to line 53, see line 53
         Scanner scanner = new Scanner(System.in);
 
         while (true) {
+            ArrayList<ToDoItem> todos = selectToDos(conn);//queries the data base each time it is looped over!
             printTodos(todos);
 
             System.out.println("Options:");
@@ -35,16 +68,18 @@ public class ToDo {
             if (optionNum == 1) {
                 System.out.println("Type a todo and hit enter");
                 String todo = scanner.nextLine();
-                ToDoItem item = new ToDoItem(todo);
-                todos.add(item);
+                //ToDoItem item = new ToDoItem(todo);
+                //todos.add(item);  We are removing this and replacing it with the insertToDo method
+                insertToDo(conn, todo); //adds ToDo method passing conn and todo into the method as the arguement
             }
             else if (optionNum == 2) {
                 System.out.println("Type the number of the todo you want to toggle");
                 String select = scanner.nextLine();
                 try {
                     int selectNum = Integer.valueOf(select);
-                    ToDoItem item = todos.get(selectNum - 1);
-                    item.isDone = !item.isDone;
+                    //ToDoItem item = todos.get(selectNum - 1); removes these items for the BOOLEAN SQL method
+                    //item.isDone = !item.isDone;
+                    toggleToDo(conn, selectNum);
                 } catch (Exception e) {
                     System.out.println("An error occurred.");
                 }
--- src/ToDoItem.java
+++ src/ToDoItem.java
@@ -2,15 +2,18 @@
  * Created by zach on 10/12/15.
  */
 public class ToDoItem {
+    int id;
     String text;
     boolean isDone;
 
     public ToDoItem(String text) {
+        this.id = id;
         this.text = text;
         this.isDone = false;
     }
 
-    public ToDoItem(String text, boolean isDone) {
+    public ToDoItem(int id, String text, boolean isDone) {
+        this.id = id;
         this.text = text;
         this.isDone = isDone;
     }
